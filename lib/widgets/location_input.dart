6a69a3e82ea9014c7c582f7ee2c933ce81e8e8d9
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-12-01 16:27:58 +0200

    add LocationInput and and fetch user coordinates

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          height: 170,
+          width: double.infinity,
+          child: _previewImageUrl == null
+              ? const Text(
+                  'No Location Chosen.',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: _getCurrentUserLocation,
+              icon: const Icon(
+                Icons.location_on,
+              ),
+              label: Text(
+                'Curent Location',
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.map,
+              ),
+              label: Text(
+                'Select on Map',
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
